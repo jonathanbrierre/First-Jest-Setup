76d1d0ce8709105aa42f826f3b8530bb19543ef7
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-06-11 21:35:35 -0400

    added some hooks and notes

--- __tests__/example.spec.js
+++ __tests__/example.spec.js
@@ -1,6 +1,35 @@
 require("../example")
 const example = window.example
 
-test('example', ()=>{
-    expect(example).toBeDefined()
+//3 A's of Testing - arrange, act, assert
+// arrange - setting up environment 
+
+    // this function is called before all the tests
+    beforeAll(()=>{
+        console.log('before all called')
+    })
+
+    //this function is called after all the tests
+    afterAll(()=>{
+        console.log('after all called')
+    })
+
+    //this function is run before each test
+    //Handy in resetting variables that tests depend on
+    beforeEach(()=>{
+        console.log('before each called')
+    })
+
+// describe gives a description of what the test does
+describe('our example module', () => {
+    test('should be defined', ()=>{
+        expect(example).toBeDefined()
+    })
+    test('should be an object', ()=>{
+        expect(typeof example).toEqual('object')
+    })
 })
+
+// use test.only to skip other tests besides that single test
+// use test.skip to skip that particular test
+
